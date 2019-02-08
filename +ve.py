1e290b935ab2ffc0767fe9cbfaca66eb54aa6943
meesam08 <47408884+meesam08@users.noreply.github.com>
2019-02-08 09:22:52 +0530

    Create +ve.py

---
+++ +ve.py
@@ -0,0 +1,16 @@
+
+while True:
+	try:
+		x = int(input())
+		break
+	except:
+		print("Invalid input")
+		break
+if x>0:
+	print('Positive')
+elif x==0:
+	print('Zero')
+else:
+	print('Negative')
+
+
